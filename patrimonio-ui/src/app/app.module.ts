1483f7e7599e246dedc356d1d104c0fa20aaab0d
adrianond <adrianond@yahoo.com.br>
2018-02-25 09:24:09 -0300

    angular

--- patrimonio-ui/src/app/app.module.ts
+++ patrimonio-ui/src/app/app.module.ts
@@ -2,18 +2,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+//para angular poder injetar a dependencia desse cara no nosso serviço item.service.ts
 import { HttpClientModule } from '@angular/common/http';
 
+//importa componentes do primeng
 import { DataTableModule, ButtonModule, InputTextModule,
   InputMaskModule, CalendarModule } from 'primeng/primeng';
 
 import { ItemService } from './item/item.service';
+//importa os componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
 
 @NgModule({
   declarations: [
+    //declara os componentes importados
     AppComponent,
     NavbarComponent,
     ItemCadastroComponent
@@ -23,7 +27,7 @@ import { ItemCadastroComponent } from './item-cadastro/item-cadastro.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
-
+    //declara os componetes importados do primeng
     DataTableModule,
     ButtonModule,
     InputTextModule,
--- patrimonio-ui/src/app/item/item.service.ts
+++ patrimonio-ui/src/app/item/item.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+//classe de serviço que cagama o back end, nosso serviço back end, a API Rest.
 @Injectable()
 export class ItemService {
 
+  //URL do nosso recurso
   itensUrl = 'http://localhost:8080/itens';
-
+  //injetando a dependencia de HttpClient para o angular instanciar ele para mim
+  //como no Spring IOC, também precisa importar este no app.module.ts
   constructor(private http: HttpClient) { }
 
+  //metodo para listar o retorno do back end, API Rest
   listar() {
+    //o angular retorna um objeto do tipo observable, esse objeto
+    //fica aguardando o retorno da chamada para nossa aplicação
+    //não ficar esperando esse retorno, ou seja, deve ser uma chamada assincrona
     return this.http.get<any[]>(this.itensUrl);
   }
 
+  //metodo para adicionar um item 
   adicionar(item: any) {
     return this.http.post(this.itensUrl, item);
   }
